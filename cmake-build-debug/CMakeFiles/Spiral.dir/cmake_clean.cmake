d8d232223d2837ad510c4ba5675dbc8e91d529a0
Mukhammet Rakhymov <samimmortal999@gmail.com>
2019-04-26 00:51:27 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Spiral.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spiral.dir/main.cpp.obj"
+  "Spiral.pdb"
+  "Spiral.exe"
+  "Spiral.exe.manifest"
+  "libSpiral.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spiral.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
